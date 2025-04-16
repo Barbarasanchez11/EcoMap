be67cccdf7a4b458d936ea08b96f77185c0085ab
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-04-16 20:51:55 +0200

    feat:logo image added

---
+++ EcoMap-Front/src/components/Header/Logo.jsx
@@ -0,0 +1,11 @@
+import '../../styles/Logo.css'
+
+const Logo = () => {
+    return (
+        <div className="logo-container">
+           <img src='https://app.logomakr.com/2INbkv/123456.png' alt="logo"></img>
+        </div>
+    );
+};
+
+export default Logo;
