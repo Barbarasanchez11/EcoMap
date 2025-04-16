eced71812fbcfe611b1860bb7ba936cd87dfedc2
Barbarasanchez11 <167989308+Barbarasanchez11@users.noreply.github.com>
2025-04-16 21:02:39 +0200

    styles:basic css for navbar

--- EcoMap-Front/src/components/Header/Logo.jsx
+++ EcoMap-Front/src/components/Header/Logo.jsx
@@ -1,9 +1,10 @@
 import '../../styles/Logo.css'
+import logo from '../../assets/logo.png'
 
 const Logo = () => {
     return (
         <div className="logo-container">
-           <img src='https://app.logomakr.com/2INbkv/123456.png' alt="logo"></img>
+          <img src={logo} alt="logo" />
         </div>
     );
 };
--- EcoMap-Front/src/components/Header/Navbar.jsx
+++ EcoMap-Front/src/components/Header/Navbar.jsx
@@ -5,10 +5,13 @@ import '../../styles/Navbar.css'
 const Navbar = () => {
     return(
         <nav>
+            <div>
+
             <ul>
               <li><Link to="/aboutus">About us</Link> </li>
               <li><Link to="contactus">Contact us</Link> </li>
             </ul>
+            </div>
         </nav>
     )
 }
